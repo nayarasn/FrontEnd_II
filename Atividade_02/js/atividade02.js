a64f07148e05cefe8c3806e5ecaad7f2160e99a4
Nayara Nascimento <127245980+nayarasn@users.noreply.github.com>
2023-09-03 21:42:59 -0300

    segunda atividade

---
+++ Atividade_02/js/atividade02.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat(prompt("Informe a primeira nota: "));
+var nota2 = parseFloat(prompt("Informe a segunda nota: "));
+var nota3 = parseFloat(prompt("Informe a terceira nota: "));
+
+var media = ((nota1*2)+(nota2*3)+(nota3*5))/(2+3+5);
+
+alert(" Sua média é: " + media);
